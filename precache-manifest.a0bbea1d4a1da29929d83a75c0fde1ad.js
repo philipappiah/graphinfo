6390d4043fd4fa96b582ff5fe8e1f2809c354c9e
philipappiah <pappiahkubi1@gmail.com>
2021-07-09 05:16:59 +0000

    Updates

---
+++ precache-manifest.a0bbea1d4a1da29929d83a75c0fde1ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937c4b5c90b6b1ac9e99e706a77db64a",
+    "url": "/graphinfo/index.html"
+  },
+  {
+    "revision": "cadcfbf93510c1ec45cd",
+    "url": "/graphinfo/static/js/2.cce8b632.chunk.js"
+  },
+  {
+    "revision": "aacb2b4ac3dc20be49bd8c86fbd4ec8e",
+    "url": "/graphinfo/static/js/2.cce8b632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c4833310812138a87e",
+    "url": "/graphinfo/static/js/main.4df621c5.chunk.js"
+  },
+  {
+    "revision": "7a89d2fdb2d80eb115a0",
+    "url": "/graphinfo/static/js/runtime-main.e53b7929.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/graphinfo/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "f14b1d2f421e14b93efb183d0cbf768b",
+    "url": "/graphinfo/static/media/logo.f14b1d2f.svg"
+  },
+  {
+    "revision": "0e48903e734324d3fa4ae30ce51535dd",
+    "url": "/graphinfo/static/media/logo_white.0e48903e.svg"
+  },
+  {
+    "revision": "3a8f3f283ff60745678c5c5613bc3f35",
+    "url": "/graphinfo/static/media/wordmark_white.3a8f3f28.svg"
+  }
+]);
